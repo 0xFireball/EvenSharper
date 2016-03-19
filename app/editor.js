e5627ac4ae96dca0e8172367876265d73280dae2
0xFireball <0xFireball@outlook.com>
2016-03-18 21:10:28 -0700

    working model

--- app/editor.js
+++ app/editor.js
@@ -12,17 +12,46 @@ function loadCode() {
     }
 }
 
+function post(path, params, method) {
+    method = method || "post"; // Set method to post by default if not specified.
+
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    for (var key in params) {
+        if (params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+        }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
+
 function compileCode() {
     postData = {
         code: textEditor.getValue(),
         lang: "csharp"
     };
-    $.post('/compile.escx', postData, function(retData) {
+    post('./compile.escx', postData);
+    /*
+    $.post('/compile.escx', postData, function(retData,status,xhr) {
+        alert(retData.url);
         var iframe = document.createElement("iframe");
         iframe.setAttribute("src", retData.url);
         iframe.setAttribute("style", "display: none");
         document.body.appendChild(iframe);
     }); 
+    */
+
 }
 
 var textArea = document.getElementById("textArea");
